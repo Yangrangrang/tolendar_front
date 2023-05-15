a927831f95a93b79ae3bf4d591ff2a169e6d1bb7
yangrangrang <gkssk2309@gmail.com>
2023-05-15 14:02:41 +0900

    signup

--- app/signup/InputForm.tsx
+++ app/signup/InputForm.tsx
@@ -13,9 +13,9 @@ export default function InputForm (props : data){
   // const [value, setValue] = useState('');
   // const idInput = useRef<HTMLInputElement>(null);
 
-  // const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   setValue(`${e.target.value}`)
-  // }
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // setValue(`${e.target.value}`)
+  }
 
   return (
     <div className="mb-2">
@@ -35,7 +35,7 @@ export default function InputForm (props : data){
             maxLength={props.max}
             pattern={props.pattern}
             // value={value}
-            // onChange={onChange}
+            onChange={onChange}
             className="block w-full px-4 py-2 mt-2 text-orange-700 bg-white border rounded-md focus:border-orange-400 focus:ring-orange-300 focus:outline-none focus:ring focus:ring-opacity-40"
         />
     </div>
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,10 +1,26 @@
 'use client'
 import { useState } from "react";
 import InputForm from "./InputForm";
+import axios from "axios";
+import { error } from "console";
+import { config } from "process";
 
 export default function LoginPage () {
+    const [validatePassword , setValidatePassword] = useState(true);
+
     const handleSubmit = async (e : React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+
+        const passwordInput = document.getElementsByName("password")[0] as HTMLInputElement;
+        const passwordCheckInput = document.getElementsByName("PasswordCheck")[0] as HTMLInputElement;
+        const password = passwordInput.value;
+        const passwordCheck = passwordCheckInput.value;
+
+        if (password !== passwordCheck) {
+            alert("비밀번호가 일치하지 않습니다.");
+            setValidatePassword(false);
+            return;
+        }
         
         // 폼 데이터 수집
         const formData = new FormData(e.currentTarget);
@@ -18,35 +34,36 @@ export default function LoginPage () {
         for (let [key, value] of formData.entries()) {
           console.log(key, value);
           json[key] = value;
+
+        // passwordCheck를 안넘기고 싶을때?
+        // const field = e.currentTarget.querySelector(`[name="${key}"]`);
+        //     if (field && !field.hasAttribute("data-ignore")) {
+        //         json[key] = value;
+        //     }
         }
 
-        // 서버로 폼 데이터 전송
-        try {
-            const response = await fetch("http://localhost:3000/api/auth/signup",{
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(json)
-            });
-
-            const data = await response.json();
-            console.log(data);
-        } catch (error){
-            console.log("test");
-            console.error(error);
-        }
-        
-        // 서버로 폼 데이터 전송
-        // fetch('/signup', {
-        //   method: 'POST',
-        //   body: formData
-        // })
-        //   .then(response => response.json())
-        //   .then(data => {
-        //     console.log(data);
-        //   })
-        //   .catch(error => {
+        // try {
+        //     const response = await axios.post('http://localhost:3000/api/users/register', json, {
+        //       headers: {
+        //         'Content-Type': 'application/json',
+        //       },
+        //     });
+        //     console.log(response.data);
+        //   } catch (error) {
         //     console.error(error);
-        //   });
+        // }
+
+        axios
+            .post("/api/users/register", json)
+            .then((response) => {
+                console.log(response);
+                alert(`${json.name}님 환영합니다.`)
+                console.log(response.data);
+                // location.replace("/login");
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     };
 
     return (
@@ -55,15 +72,16 @@ export default function LoginPage () {
                 <h1 className="text-3xl font-semibold text-center text-orange-400 underline">
                    Sign up
                 </h1>
-                <form action={"/api/auth/signup"} method="post" className="mt-6" onSubmit={(e) => {
+                <form action={"/api/users/register"} method="post" className="mt-6" onSubmit={(e) => {
                     e.preventDefault();
                     handleSubmit(e);
                 }}>
-                    < InputForm name = "ID" type = "text" />
-                    < InputForm name = "Password" type = "password" min = {8} max = {20} pattern="[a-z0-9]{1,15}"/>
-                    < InputForm name = "Password check" type = "password" min = {8} max = {20} pattern="[a-z0-9]{1,15}"/>
-                    < InputForm name = "Name" type = "text"/>
-                    < InputForm name = "Email" type = "email"/>
+                    < InputForm name = "username" type = "text" />
+                    < InputForm name = "password" type = "password" min = {8} max = {20} pattern="[a-z0-9]{1,15}"/>
+                    < InputForm name = "PasswordCheck" type = "password" min = {8} max = {20} pattern="[a-z0-9]{1,15}" data-ignore/>
+                    {validatePassword? " " : <p>비밀번호가 일치 하지 않습니다.</p>}
+                    < InputForm name = "name" type = "text"/>
+                    < InputForm name = "email" type = "email"/>
                     <div className="mt-6">
                         <button type="submit" className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-orange-400 rounded-md hover:bg-orange-300 focus:outline-none focus:bg-orange-500">
                             Sign up
