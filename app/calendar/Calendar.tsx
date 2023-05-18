b456e34042966f4dbe4e3d61705c869cc22ef9d5
yangrangrang <gkssk2309@gmail.com>
2023-05-19 08:56:23 +0900

    calendar start

--- app/calendar/Calendar.tsx
+++ app/calendar/Calendar.tsx
@@ -5,36 +5,50 @@ import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import interactionPlugin from "@fullcalendar/interaction"
 import timeGridPlugin from "@fullcalendar/timegrid";
 import DayCalendar from './DayCalendar'
+import { DateSelectArg } from '@fullcalendar/core'
 
 export default function Calendar(props: any) {
 
-  const handleDateClick = (arg: any) => { // bind with an arrow function
-    if(!props.onDateClick) {return;}
-    props.onDateClick(arg)
-  }
+    const handleDateClick = (arg: any) => { // bind with an arrow function
+        if(!props.onDateClick) {return;}
+        props.onDateClick(arg)
+    }
 
-  return (
-    <div className="p-3 border">
-      <FullCalendar
-        // defaultView="dayGridMonth" 
-        plugins={[ interactionPlugin , dayGridPlugin, timeGridPlugin ]}
-        headerToolbar={{
-          left: "prev,next today",
-          center: "title",
-          right: "dayGridMonth,timeGridWeek,timeGridDay"
-        }}
-        weekends={false}
-        eventClick={() => {
-          alert('event')
-        }}
-        events={[
-            { title: 'event 1', date: '2023-05-01' },
-            { title: 'event 2', date: '2023-05-02' },
-            { title: 'event 3', start: '2023-05-03', end: '2023-05-06' },
-        ]}
-        height={"500px"}
-        dateClick={handleDateClick}
-      />
-    </div>
-  )
+    const handleDateSelect = (selectInfo: DateSelectArg) => {
+        const startDate = selectInfo.startStr; // 클릭한 날짜의 시작일
+        const endDate = selectInfo.endStr; // 클릭한 날짜의 종료일 (일정의 경우에는 동일한 날짜)
+
+        // 여기에 선택한 날짜에 대한 일정 등록 로직을 작성
+        // 서버로 요청을 보내거나 상태를 업데이트하고 등등...
+
+        console.log('Selected Date Range:', startDate, ' - ', endDate);
+    }
+
+    return (
+        <div className="p-3 border">
+        <FullCalendar
+            // defaultView="dayGridMonth" 
+            plugins={[ interactionPlugin , dayGridPlugin, timeGridPlugin ]}
+            headerToolbar={{
+            left: "prev,next today",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay"
+            }}
+            weekends={true}
+            eventClick={() => {
+            alert('event')
+            }}
+            events={[
+                { title: 'event 1', date: '2023-05-01' },
+                { title: 'event 2', date: '2023-05-02' },
+                { title: 'event 3', start: '2023-05-03', end: '2023-05-06' },
+            ]}
+            selectable = {true}
+            // selectOverlap = {selectEvent}
+            height={"500px"}
+            select={handleDateSelect}
+            dateClick={handleDateClick}
+        />
+        </div>
+    )
 }
--- app/calendar/DayCalendar.tsx
+++ app/calendar/DayCalendar.tsx
@@ -4,12 +4,11 @@ export default function DayCalendar (props: any){
         console.log("click");
     }
 
-
     return (
         <div className="todolist-container h-60 px-3 mt-1 bg-orange-50 rounded-md">
             <div className="flex justify-between py-3" >
                 <p>{props.date}</p>
-                <button onClick={handlerClick} className="px-2">+</button>
+                <button className="px-3 py-1 ml-2 border rounded-full border-orange-400 text-orange-600" onClick={handlerClick}>등록</button>
             </div>
 
             <ul>
